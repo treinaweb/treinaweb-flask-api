1a0c67626a3b2357e81ccca87c5381c4b9e56647
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-10-03 15:03:25 -0300

    05.10_Relacionar_Funcionario_Projeto

--- api/models/funcionario_model.py
+++ api/models/funcionario_model.py
@@ -1,4 +1,5 @@
 from api import db
+from .projeto_model import Projeto
 
 class Funcionario(db.Model):
     __tablename__ = "funcionario"
@@ -6,3 +7,5 @@ class Funcionario(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     nome = db.Column(db.String(50), nullable=False)
     idade = db.Column(db.Integer, nullable=False)
+
+    projetos = db.relationship(Projeto, secondary="funcionario_projeto", back_populates="funcionarios")
--- api/models/projeto_model.py
+++ api/models/projeto_model.py
@@ -1,4 +1,11 @@
 from api import db
+from .funcionario_model import Funcionario
+
+funcionario_projeto = db.Table('funcionario_projeto',
+               db.Column('projeto_id', db.Integer, db.ForeignKey('projeto.id'), primary_key=True, nullable=False),
+               db.Column('funcionario_id', db.Integer, db.ForeignKey('funcionario.id'), primary_key=True, nullable=False)
+                               )
+
 
 class Projeto(db.Model):
     __tablename__ = "projeto"
@@ -6,3 +13,5 @@ class Projeto(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     nome = db.Column(db.String(50), nullable=False)
     descricao = db.Column(db.String(100), nullable=False)
+
+    funcionarios = db.relationship(Funcionario, secondary="funcionario_projeto", back_populates="projetos")
