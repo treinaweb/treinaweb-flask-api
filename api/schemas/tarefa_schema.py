ae360f8cda4fa1075dca792b14f7bc4fca5d3ab3
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-10-08 09:38:50 -0300

    08.02_Implementando_HATEOAS

--- api/schemas/tarefa_schema.py
+++ api/schemas/tarefa_schema.py
@@ -5,9 +5,17 @@ from marshmallow import fields
 class TarefaSchema(ma.ModelSchema):
     class Meta:
         model = tarefa_model.Tarefa
-        fields = ("id", "titulo", "descricao", "data_expiracao", "projeto")
+        fields = ("id", "titulo", "descricao", "data_expiracao", "projeto", "_links")
 
     titulo = fields.String(required=True)
     descricao = fields.String(required=True)
     data_expiracao = fields.Date(required=True)
     projeto = fields.String(required=True)
+    _links = ma.Hyperlinks(
+        {
+            "get": ma.URLFor("tarefadetail", id="<id>"),
+            "put": ma.URLFor("tarefadetail", id="<id>"),
+            "delete": ma.URLFor("tarefadetail", id="<id>"),
+
+        }
+    )
