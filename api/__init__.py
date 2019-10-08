89589170cf0967351929040f84072499dcfbfef0
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-10-08 09:48:17 -0300

    09.02_Instalando_Swagger

--- api/__init__.py
+++ api/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager
+from flasgger import Swagger
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -13,6 +14,7 @@ migrate = Migrate(app, db)
 JWTManager(app)
 
 api = Api(app)
+swagger = Swagger(app)
 
 from .views import tarefa_views, projeto_views, funcionario_views, usuario_views, login_views
 from .models import tarefa_model, projeto_model, funcionario_model, usuario_model
